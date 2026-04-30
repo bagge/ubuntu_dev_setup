5c1969c19e3ab231eb92e545a8c15b37d3501115
bagge <alexander.szakaly@gmail.com>
2026-04-30 21:52:07 +0200

    First attempt at mac OS support

--- tests/shell/check.sh
+++ tests/shell/check.sh
@@ -5,8 +5,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 bash_files=(
     bashrc
+    bash_profile
     create_bundle.sh
     bashrc.d/000-setup-location.bashrc
+    bashrc.d/001-homebrew.bashrc
     bashrc.d/002-bash_it.bashrc
     bashrc.d/003-bazel-helpers.bashrc
     bashrc.d/004-bash-history.bashrc
@@ -16,6 +18,7 @@ bash_files=(
     bashrc.d/999-optional-auth.bashrc
     tests/container/run.sh
     tests/container/run-in-container.sh
+    tests/macos/run.sh
     tests/shell/check.sh
 )
 
@@ -27,5 +30,6 @@ if command -v shellcheck >/dev/null 2>&1; then
     shellcheck \
         tests/container/run.sh \
         tests/container/run-in-container.sh \
+        tests/macos/run.sh \
         tests/shell/check.sh
 fi
